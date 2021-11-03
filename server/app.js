1914fcf0bba9190f0c7fb927999abeb78fa619b4
Diony Severino (RIT Student) <dls2307@rit.edu>
2021-11-03 13:57:58 -0400

    Finished Domomaker C

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const url = require('url');
 const redis = require('redis');
+const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -24,20 +25,20 @@ mongoose.connect(dbURL, (err) => {
 
 let redisURL = {
   hostname: 'redis-17640.c291.ap-southeast-2-1.ec2.cloud.redislabs.com',
-  port:'17640',
+  port: '17640',
 };
 
 let redisPASS = 'uO3QjcFiApl0CH5sU5v4PrV6b96dWA9r';
 
-if(process.env.REDISCLOUD_URL){
+if (process.env.REDISCLOUD_URL) {
   redisURL = url.parse(process.env.REDISCLOUD_URL);
-  [,redisPASS] = redisURL.auth.split(':');
+  [, redisPASS] = redisURL.auth.split(':');
 }
 
-let redisClient = redis.createClient({
-  host:redisURL.hostname,
-  port:redisURL.port,
-  password:redisPASS,
+const redisClient = redis.createClient({
+  host: redisURL.hostname,
+  port: redisURL.port,
+  password: redisPASS,
 });
 
 const router = require('./router.js');
@@ -52,19 +53,26 @@ app.use(bodyParser.urlencoded({
 app.use(session({
   key: 'sessionid',
   store: new RedisStore({
-    client:redisClient,
+    client: redisClient,
   }),
   secret: 'Domo Arigato',
   resave: true,
   saveUninitialized: true,
-  cookie:{
-    httpOnly:true,
+  cookie: {
+    httpOnly: true,
   },
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
+app.disable('x-powered-by');
 app.use(cookieParser());
+app.use(csrf());
+app.use((err, req, res, next) => {
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
+  console.log('Missing CSRF token');
+  return false;
+});
 
 router(app);
 
